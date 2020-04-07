2902c553cee48cce73b514480ccec2c90623a6f1
shaheer1995 <mohomed.shaheer@gmail.com>
2020-04-07 18:53:48 +0530

    updated libraries

--- utils/ops.py
+++ utils/ops.py
@@ -26,10 +26,10 @@ from six.moves import range
 from six.moves import zip
 import tensorflow as tf
 
-from signal_detection.utils import standard_fields as fields
-from signal_detection.utils import shape_utils
-from signal_detection.utils import spatial_transform_ops as spatial_ops
-from signal_detection.utils import static_shape
+from utils import standard_fields as fields
+from utils import shape_utils
+from utils import spatial_transform_ops as spatial_ops
+from utils import static_shape
 
 
 matmul_crop_and_resize = spatial_ops.matmul_crop_and_resize
